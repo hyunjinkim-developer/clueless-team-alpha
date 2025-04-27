c9567635272a756af86a03ea2b9c0aef89c5cd05
RiaJ712 <rjain31@jh.edu>
2025-04-27 19:16:29 -0400

    dining room was two words in ROOMS but one word in ADJACENCY

--- game/constants.py
+++ game/constants.py
@@ -48,8 +48,8 @@ ADJACENCY = {
     'Hall': ['Hallway1', 'Hallway2', 'Hallway4'],
     'Lounge': ['Hallway2', 'Hallway5', 'Conservatory'], # Secret passage to Conservatory
     'Library': ['Hallway3', 'Hallway6', 'Hallway8'],
-    'BilliardRoom': ['Hallway4', 'Hallway6', 'Hallway7', 'Hallway9'],
-    'DiningRoom': ['Hallway5', 'Hallway7', 'Hallway10'],
+    'Billiard Room': ['Hallway4', 'Hallway6', 'Hallway7', 'Hallway9'],
+    'Dining Room': ['Hallway5', 'Hallway7', 'Hallway10'],
     'Conservatory': ['Hallway8', 'Hallway11', 'Lounge'],  # Secret passage to Lounge
     'Ballroom': ['Hallway9', 'Hallway11', 'Hallway12'],
     'Kitchen': ['Hallway10', 'Hallway12', 'Study'],  # Secret passage to Study
@@ -57,13 +57,13 @@ ADJACENCY = {
     'Hallway1': ['Study', 'Hall'],
     'Hallway2': ['Hall', 'Lounge'],
     'Hallway3': ['Study', 'Library'],
-    'Hallway4': ['Hall', 'BilliardRoom'],
-    'Hallway5': ['Lounge', 'DiningRoom'],
-    'Hallway6': ['Library', 'BilliardRoom'],
-    'Hallway7': ['BilliardRoom', 'DiningRoom'],
+    'Hallway4': ['Hall', 'Billiard Room'],
+    'Hallway5': ['Lounge', 'Dining Room'],
+    'Hallway6': ['Library', 'Billiard Room'],
+    'Hallway7': ['Billiard Room', 'Dining Room'],
     'Hallway8': ['Library', 'Conservatory'],
-    'Hallway9': ['BilliardRoom', 'Ballroom'],
-    'Hallway10': ['DiningRoom', 'Kitchen'],
+    'Hallway9': ['Billiard Room', 'Ballroom'],
+    'Hallway10': ['Dining Room', 'Kitchen'],
     'Hallway11': ['Conservatory', 'Ballroom'],
     'Hallway12': ['Ballroom', 'Kitchen']
 }
