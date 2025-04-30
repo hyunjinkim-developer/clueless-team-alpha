8198b36f93aabdb1768fadd2a92d523838e01242
RiaJ712 <rjain31@jh.edu>
2025-04-29 21:03:48 -0400

    printing list of players to check cards for suggestion

--- game/consumers.py
+++ game/consumers.py
@@ -393,6 +393,7 @@ class GameConsumer(AsyncWebsocketConsumer):
         playerCopyList.remove(suspect_player)
         playerCopyList.remove(player)
         playerSuggestList.extend(playerCopyList)
+        print(f"list of players to check cards for suggestion: {playerSuggestList}")
 
         # check suspected player's hand for suspect, then weapon, then room
         # put all matches into separate list
